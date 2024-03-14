bcaf173e45f77ecf0cff32cb9657196ff61bb94d
Mustansir Bharmal <112817095+mustansirbharmal-portfolio@users.noreply.github.com>
2024-03-14 14:45:42 +0530

    wfc
    wfc

---
+++ search.js
@@ -0,0 +1,17 @@
+// search results //
+function myFunction() {
+    var input, filter, cards, cardContainer, h5, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("myItems");
+    cards = cardContainer.getElementsByClassName("card");
+    for (i = 0; i < cards.length; i++) {
+        title = cards[i].querySelector(".card-title");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].parentElement.style.display = "flex"
+              } else {
+                cards[i].parentElement.style.display = "none"
+        }
+    }
+}
+
